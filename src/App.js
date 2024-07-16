6fde077970da087a741bc233e0ca6e04b0845b20
bhaskardhuri <bhaskardhuri641@gmail.com>
2024-07-16 17:57:30 +0530

    Added TextArea

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import Header from './component/Header/Header';
 import Footer from './component/Footer/Footer';
 import Message from './component/Message/Message';
+import TextArea from './component/TextArea/TextArea';
 import './global.css';
 
 function App() {
   return (
     <div className="App">
      <Header/>
-     <Message/>  
+     {/* <Message/> */}
+     <TextArea/>  
      <Footer/>
    </div>
   );
