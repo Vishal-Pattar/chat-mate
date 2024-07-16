ae820745fe6246db68527878213d905c772a80ba
bhaskardhuri <bhaskardhuri641@gmail.com>
2024-07-16 12:52:42 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './component/Header/Header';
+import Footer from './component/Footer/Footer';
+import './global.css';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+     <Header/>
     </div>
   );
 }
