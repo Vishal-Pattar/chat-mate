313a3eac6f84321d3eea34a02037aaec6739bcc1
bhaskardhuri <bhaskardhuri641@gmail.com>
2024-07-16 16:48:08 +0530

    Updated Display Box

--- src/component/DisplayBox/DisplayBox.jsx
+++ src/component/DisplayBox/DisplayBox.jsx
@@ -4,8 +4,10 @@ import './DisplayBox.css';
 const DisplayBox = () => {
   return (
     <div className='displaybox__container'>
-        <div className='displaybox__sendermessage'>Hello World from Sender</div>
-        <div className='displaybox__recievermessage'>Hello World from Reciever</div>
+      <div className='displaybox__message '>
+        <div className='displaybox__sendermessage  usermessage'>Hello World from Sender</div>
+        <div className='displaybox__recievermessage usermessage'>Hello World from Reciever</div>
+      </div>
     </div>
   )
 }
