9fb71153a3c24f93a9c3afb9943aa8c53b95d2dd
bhaskardhuri <bhaskardhuri641@gmail.com>
2024-07-16 13:19:01 +0530

    Updated Footer.jsx

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -3,8 +3,8 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className='footer__container'>
+      <div className='footer__title'>&copy; Copyright All Right Reserved 2024, Chat-mate, Pune.</div>
     </div>
   )
 }
