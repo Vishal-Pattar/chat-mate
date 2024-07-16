ef1745a40d2f642c013183a44d4d812236534927
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-07-16 16:31:08 +0530

    Updated Header

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -4,7 +4,7 @@ import './Header.css'
 const Header = () => {
   return (
     <div className='header__container'>
-        <div className='header__title'>Chat-Mate</div>
+        <div className='header__title'>Chat Mate</div>
     </div>
   )
 }
