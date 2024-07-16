e7de33ab60e93dbf42d23b391fcac579b14cdc85
bhaskardhuri <bhaskardhuri641@gmail.com>
2024-07-16 12:49:57 +0530

    Added Header

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header__container'>
+        <div className='header__title'>Chat-Mate</div>
+    </div>
+  )
+}
+
+export default Header
