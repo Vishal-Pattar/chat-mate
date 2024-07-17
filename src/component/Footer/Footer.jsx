c6f5d7bd8263c12f01bf9ad4998041bf99856177
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-07-17 15:02:48 +0530

    Updated Footer

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className='footer__container'>
       <div className='footer__title'>&copy; Copyright All Rights Reserved 2024, Chat Mate, Pune.</div>
-      <div className='footer__title'>Made By <a href='#'>Vishal Pattar</a> and <a href="#">Bhaskar Dhuri</a></div>
+      <div className='footer__title'>Made By <a href='https://github.com/Vishal-Pattar'>Vishal Pattar</a> and <a href="https://github.com/bhaskardhuri">Bhaskar Dhuri</a></div>
     </div>
   )
 }
