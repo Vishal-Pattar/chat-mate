ea04f7c1cb95b8448f1eb96a52be55f31a64cdf5
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-07-17 17:10:34 +0530

    Updated TextArea

--- src/component/TextArea/TextArea.jsx
+++ src/component/TextArea/TextArea.jsx
@@ -1,15 +1,40 @@
-import React from 'react'
-import './TextArea.css'
+import React, { useState, useEffect, useRef } from 'react';
+import './TextArea.css';
 import { IoMdSend } from "react-icons/io";
+
 const TextArea = () => {
+  const [content, setContent] = useState('');
+  const textAreaRef = useRef(null);
+
+  useEffect(() => {
+    if (textAreaRef.current) {
+      textAreaRef.current.style.height = 'auto';
+      textAreaRef.current.style.height = textAreaRef.current.scrollHeight + 'px';
+    }
+  }, [content]);
+
+  const handleChange = (e) => {
+    setContent(e.target.value);
+  };
+
   return (
     <div className="textarea__container">
-      <input type="text" placeholder="Enter your message" className='textarea__input' />
-      <button className='textarea__btn'>
-      <IoMdSend />
-      </button>
+      <div className='textarea__box'>
+        <textarea
+          ref={textAreaRef}
+          type="text"
+          placeholder="Enter your message"
+          className='textarea__input'
+          rows={1}
+          value={content}
+          onChange={handleChange}
+        ></textarea>
+        <button className='textarea__btn'>
+          <IoMdSend className='textarea__sendicon' />
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TextArea
+export default TextArea;
