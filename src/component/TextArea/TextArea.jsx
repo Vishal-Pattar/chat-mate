505e03f6d357510c84a40d3ec685932f85e6e2c4
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-07-17 15:53:09 +0530

    Updated TextArea

--- src/component/TextArea/TextArea.jsx
+++ src/component/TextArea/TextArea.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 import './TextArea.css'
 const TextArea = () => {
   return (
-        <div className="textarea__container">
-        <input type="text" placeholder="Enter your message" className='textarea__input'/>
-        <button className='textarea__btn'>
-            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28"
-                viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-                stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                <path d="M5 12l14 0"></path>
-                <path d="M13 18l6 -6"></path>
-                <path d="M13 6l6 6"></path>
-            </svg>
-        </button>
+    <div className="textarea__container">
+      <input type="text" placeholder="Enter your message" className='textarea__input' />
+      <button className='textarea__btn'>
+        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28"
+          viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+          stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+          <path d="M5 12l14 0"></path>
+          <path d="M13 18l6 -6"></path>
+          <path d="M13 6l6 6"></path>
+        </svg>
+      </button>
     </div>
   )
 }
